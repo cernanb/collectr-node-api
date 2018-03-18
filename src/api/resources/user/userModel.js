080ef3ca8a735ed22b990320ee756ac953fb544e
Cernan Bernardo <cernanb@gmail.com>
2018-03-17 17:45:05 -0700

    Add username and timestamps to user schema

--- src/api/resources/user/userModel.js
+++ src/api/resources/user/userModel.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    unique: true,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: { type: String },
   },
-  password: { type: String },
-})
+  { timestamps: true }
+)
 
 const User = mongoose.model('user', userSchema)
 
