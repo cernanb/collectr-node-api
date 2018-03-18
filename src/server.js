b22cbd478eaec7993ff01c4ff0c2b9bf34f22e0b
Cernan Bernardo <cernanb@gmail.com>
2018-03-17 17:10:38 -0700

    Set up global middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const setupMiddleware = require('./middleware')
+const { connect } = require('./db')
+const User = require('./api/resources/user/userModel')
 
 const app = express()
+
+setupMiddleware(app)
+connect()
 
 module.exports = app
